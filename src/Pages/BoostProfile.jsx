c345c35c0a67463d13f8367e76400185e9eecb08
Saabika Roshni S <saabikaroshni@gmail.com>
2025-06-13 18:58:16 +0530

    boostprofile

--- src/Pages/BoostProfile.jsx
+++ src/Pages/BoostProfile.jsx
@@ -2,18 +2,42 @@ import React from 'react';
 import "../Styles/boostprofile.css"
 const BoostProfile = () => {
   return (
-    <div>
+    <div className='container'>
         
         <div className='container'>
           <h2>SELECT BOOST TYPE</h2>
-          <h3>FEATURED PROFILES</h3>
-          <div className='container1'>
-            
-          </div>
-          <h3>SEARCH PREFERENCES</h3>
-          <div className='container2'>
+         <div className='container-col'>
+            <div className='container-col'>
+              <h3>FEATURED PROFILES</h3>
+              <p>Be showcased on the home page to every other user and also
+                increases your visibility on the search page</p>
+              <div className='container1'>
+                <h3>FEATURED PROFILES</h3>
+                <div className='container'>
+                  <div className='container3'>
 
-          </div>
+                </div>
+                <div className='container3'>
+
+                </div>
+                <div className='container3'>
+
+                </div>
+                </div>
+                
+              </div>
+              <h3>Rs 150/day</h3>
+            </div>
+            <div className='container-col'>
+              <h3>SEARCH PREFERENCES</h3>
+              <p>Increase your visibility by being on top of the 
+                search page in your industry</p>
+              <div className='container2'>
+                <h3>SEARCH PREFERENCES</h3>
+              </div>
+              <h3>Rs 100/day</h3>
+            </div>
+         </div>
         </div>
     </div>
   )
